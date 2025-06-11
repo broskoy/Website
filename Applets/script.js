9a8a8e425ed5b6fac53bccb1160c2e2b97801726
broskoy <mihnea.buzoiu@gmail.com>
2025-06-11 23:01:36 +0200

    made radius into applets + big upgrade

---
+++ Applets/script.js
@@ -0,0 +1,7 @@
+// update the value displayed under the slider
+function sliderChanged(event) {
+    let newValue = event.target.value;
+    let element = document.getElementById("slider-label");
+    element.textContent = newValue;
+    document.documentElement.style.setProperty('--radius', `${newValue}px`);
+}
