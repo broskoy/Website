e71276f0c33595a7cd49d00aa2cfc261f806e006
broskoy <mihnea.buzoiu@gmail.com>
2025-07-15 20:19:01 +0300

    applets demo is complete

--- Applets/script.js
+++ Applets/script.js
@@ -3,5 +3,5 @@ function sliderChanged(event) {
     let newValue = event.target.value;
     let element = document.getElementById("slider-label");
     element.textContent = newValue;
-    document.documentElement.style.setProperty('--radius', `${newValue}px`);
+    document.documentElement.style.setProperty('--radius', `${newValue / 100 * 4}vh`);
 }
